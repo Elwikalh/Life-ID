3558c0c2ff075fdb77f1f3f157f2bec7a2665a4b
Elwikalh <ahmedkamhawyno1@gmail.com>
2026-07-21 16:22:50 +0300

    Create page.tsx

---
+++ apps/web/src/app/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import { SignUp } from "@clerk/nextjs"
+
+export default function Page() {
+  return (
+    <div className="flex min-h-[80vh] items-center justify-center p-6">
+      <SignUp routing="hash" />
+    </div>
+  )
+}
